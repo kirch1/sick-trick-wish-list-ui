7f9e68e0dc03a4b09135e2c4f78f3faa5d15f7db
Jason Kirchman <kirchman.jason@icloud.com>
2023-04-10 10:34:56 -0500

    Add starting css for Card and CardContainer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 function Card({trick}) {
   return(
     <div className='card'>
-      <p>{trick.name}</p>
+      <p>{trick.stance} {trick.name}</p>
       <p>Obstacle: {trick.obstacle}</p>
       <p>Link to Tutorial: {trick.tutorial}</p>
     </div>
